48fc3a1d29cc688d326dbf91637cf33ef70a095c
cxr2 <ciosahoulihan@gmail.com>
2022-09-04 13:24:48 +0100

    added rsvp

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg">
           <div className="container-fluid mx-5 px-5 py-3">
             <a className="navbar-brand" href="#">
               PartyStarter
@@ -41,8 +41,53 @@ function App() {
         <div className="col-12">
           <img src={Party} className="d-block mx-auto" alt="party" />
           <h1 className="text-center">22 DAYS 12 HOURS 13 SECONDS</h1>
+          <button
+            className="btn d-block mx-auto px-4 mt-4"
+            id="rsvp"
+            href="mailto: rsvp@party.com"
+          >
+            RSVP
+          </button>
         </div>
       </div>
+      {/* <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Understood
+              </button>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 }
