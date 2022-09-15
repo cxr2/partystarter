2326704a27af1e8f74a483a9db5a1f607eccc601
cxr2 <ciosahoulihan@gmail.com>
2022-09-15 18:06:11 +0100

    Working raffle added

--- src/components/forms/RaffleForm.jsx
+++ src/components/forms/RaffleForm.jsx
@@ -11,14 +11,21 @@ function RaffleForm() {
   const [name, setName] = useState("");
 
   const listItems = nameList.map((name) => (
-    <li className="capitalize" key={uuidv4()}>
-      {name}
-    </li>
+    <div
+      className="capitalize m-2 col-2 d-flex justify-content-center"
+      key={uuidv4()}
+    >
+      <span className="singlename p-2">{name}</span>
+    </div>
   ));
 
   // const startRaffle = () => {
   //   if (listItems.length >= 10) return <button>start raffle</button>;
   // };
+
+  let winner = nameList[Math.floor(Math.random() * nameList.length)];
+
+  console.log(winner);
 
   const handleChange = (event) => {
     setName(event.target.value);
@@ -52,41 +59,82 @@ function RaffleForm() {
       <div className="form-container">
         <form onSubmit={handleSubmit}>
           <div className="row justify-content-center">
-            <div className="col-6 ">
-              <label htmlFor="fullName" className="p-2">
+            <div className="col-6">
+              <label htmlFor="fullName" className="p-2 namelabel">
                 Enter your full name
               </label>
-            </div>
-          </div>
-          <div className="row justify-content-center pb-3">
-            <div className="col-6">
+
               <input
                 type="text"
                 name="fullName"
-                className="p-3"
+                className="p-3 fullname"
                 placeholder="Full name"
                 onChange={handleChange}
                 value={name || ""}
                 required
               />
             </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-6">
-              <button
-                type="submit"
-                className="btn btn-primary m-2"
-                onChange={handleSubmit}
-              >
-                Submit
-              </button>
+            <div className="row justify-content-center">
+              <div className="col-6">
+                <button
+                  type="submit"
+                  className="btn rafsub btn-primary mt-3"
+                  onChange={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </form>
       </div>
-      <div className="nameDisplay">
-        <ul>{listItems}</ul>
-        {nameList?.length >= 10 && <button>start raffle</button>}
+      <div className="nameDisplay container pt-5">
+        <div className="row justify-content-center">
+          {/* <ul className="rafflelist list-group list-group-horizontal"> */}
+          {listItems}
+          {/* </ul> */}
+        </div>
+        <div className="row justify-content-center">
+          {nameList?.length >= 10 && (
+            <div className="col-6 p-5">
+              <button
+                type="button"
+                className="startraffle py-1 px-3"
+                data-bs-toggle="modal"
+                data-bs-target="#raffleWinner"
+              >
+                Start Raffle
+              </button>
+            </div>
+          )}
+
+          <div className="modal" id="raffleWinner">
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title">Congratulations!</h4>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                  ></button>
+                </div>
+
+                <div className="modal-body capitalize">{winner}</div>
+
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
